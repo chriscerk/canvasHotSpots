af237279140af82b77a81064a7957b89d423ad28
Cerk <chrcerk@UMICH.EDU>
2016-11-29 15:08:43 -0500

    Restructing into module format

--- example/hotspotsExample.js
+++ example/hotspotsExample.js
@@ -1,17 +1,34 @@
-var elements = [
+var elementsOne = [
     {
         name: 'Ring A',
-        y: 234,
-        x: 118,
+        y: 263,
+        x: 292,
         size: 60,
         text: "Pi-Pi stacking with BZR"
   },
   {
         name: 'Ring B',
-        y: 208,
-        x: 231,
-        size: 70,
+        y: 280,
+        x: 203,
+        size: 60,
         text: "H-bond acceptor (O, N, S) or N-heterocycle required. 1,2 annelation (N-heterocycle) increases affinity for BZR"
+  },
+];
+
+var elementsTwo = [
+    {
+        name: '',
+        y: 50,
+        x: 140,
+        size: 30,
+        text: "R"
+  },
+  {
+        name: '',
+        y: 37,
+        x: 284,
+        size: 30,
+        text: "O"
   },
 ];
 
@@ -24,4 +41,6 @@ var events = [
 //Example Image
 var image = "benzodiazepine.PNG";
 
-hotspots.initCanvas(elements, events);
+hotspots.initCanvasImg("myCanvas", image, 150, 200, elementsOne);
+
+hotspots.initCanvasImg("myOtherCanvas", image, 10, 300, elementsTwo);
--- hotspots.js
+++ hotspots.js
@@ -1,51 +1,116 @@
 //Globals
-var canvas = document.getElementById('myCanvas');
-var context = canvas.getContext('2d');
-var imageObj = new Image();
+var canvases = [];
+var contexts = [];
+var imageObjs = [];
 
 var hotspots = {
-    initCanvas: function(elements, events) {
-        imageObj.onload = function() {
-            var imgWidth = this.width;
-            var imgHeight = this.height;
-
-            initCanvasSize(imgWidth, imgHeight);
-            refreshCanvas();
-            initEvents(events, elements);
-        };
-        imageObj.src = image;
-        console.log(imageObj);
+    initCanvas: function(canvasId) {
+      canvases[canvasId] = document.getElementById(canvasId);
+      contexts[canvasId] = canvases[canvasId].getContext('2d');
+      imageObjs[canvasId] = new Image();
     },
-    initDialogue: function(x, y, height, width) {
+
+    initCanvasWithDims: function(canvasId, width, height) {
+      hotspots.initCanvas(canvasId);
+      setCanvasSize(canvasId, width, height);
+      refreshCanvas(canvasId, padding);
+    },
+
+    initCanvasImg: function(canvasId, image) {
+      hotspots.initCanvas(canvasId);
+
+      imageObj.onload = function() {
+          var imgWidth = this.width;
+          var imgHeight = this.height;
+
+          setCanvasSize(imgWidth, imgHeight);
+          refreshCanvas(canvasId, padding);
+      };
+      imageObj.src = image;
+    },
+
+    initCanvasImg: function(canvasId, image, padding, desiredImgWidth, elements) {
+      hotspots.initCanvas(canvasId);
+
+      imageObjs[canvasId].onload = function() {
+          var imgWidth = this.width;
+          var imgHeight = this.height;
+
+          var calcImgHeight = getCalculatedHeight(imgWidth, imgHeight, desiredImgWidth);
+
+          var canvasHeight = padding*2 + calcImgHeight;
+          var canvasWidth = padding*2 + desiredImgWidth;
+
+          setCanvasSize(canvasId, canvasWidth, canvasHeight);
+          refreshCanvas(canvasId, padding);
+      };
+      imageObjs[canvasId].src = image;
+
+      hotspots.initHotSpotEvents(canvasId, events, elements);
+
+      canvases[canvasId].onmousemove=function(e) {
+        var isHotspot = checkIfHotspot(canvasId, e, padding, elements);
+        if(isHotspot) {
+          console.log('HOTSPOT!');
+        }
+      };
+    },
+
+    displayDialogueBox: function(canvasId, size, padding){
+      var dialogueSize = size;
+      var originalCanvasHeight = contexts[canvasId].canvas.height;
+      var originalCanvasWidth = contexts[canvasId].canvas.width;
+
+      setCanvasSize(canvasId, originalCanvasWidth, originalCanvasHeight + dialogueSize);
+      refreshCanvas(canvasId, padding);
+    },
+
+    initHotSpotEvents: function(canvasId, events, elements) {  
+
+      events.forEach(function(e) {
+        canvases[canvasId].addEventListener(e, function(event) {
+          var x = event.pageX - canvases[canvasId].offsetLeft;
+          var y = event.pageY - canvases[canvasId].offsetTop;
+           console.log(
+                e + 
+                ' @ (' + x + ',' + y + ')'
+              );   
+
+          elements.forEach(function(el) {
+            if (y > el.y - el.size && 
+                y < el.y + el.size && 
+                x > el.x - el.size && 
+                x < el.x + el.size) {
+              console.log(
+                  e +
+                ' element:' + 
+                el.name + 
+                ' @ (' + x + ',' + y + ')'
+              );       
+            }
+          });
+        }, false);
         
-    },
-};
-
-canvas.onmousemove=function(e){
-    var isHotspot = checkIfHotspot(e);
-    if(isHotspot) {
-     console.log('HOTSPOT!');
+      });
   }
 };
 
-function initCanvasSize(imgWidth, imgHeight) {
-    var padding = 50;
+function getCalculatedHeight(imgWidth, imgHeight, desiredImgWidth) {
 
-    var total = imgWidth + imgHeight;
-    var myImgWidth = 250;
+    var yxRatio = imgHeight / imgWidth;
+    var myImgHeight = desiredImgWidth * yxRatio;
 
-    var xRatio = imgWidth / total;
-    var yRatio = imgHeight / total;
-
-    var myImgHeight = myImgWidth / xRatio;
-
-    context.canvas.width = myImgWidth + padding*2;
-    context.canvas.height = myImgHeight + padding*2;
+    return myImgHeight;
 }
 
-function checkIfHotspot(e) {
-    var x = e.clientX - canvas.offsetLeft;
-    var y = e.clientY - canvas.offsetTop;
+function setCanvasSize(canvasId, width, height) {
+    contexts[canvasId].canvas.width = width;
+    contexts[canvasId].canvas.height = height;
+}
+
+function checkIfHotspot(canvasId, e, padding, elements) {
+    var x = e.clientX - canvases[canvasId].offsetLeft;
+    var y = e.clientY - canvases[canvasId].offsetTop;
     var isHotspot = false;
 
     elements.forEach(function(el) {
@@ -54,9 +119,9 @@ function checkIfHotspot(e) {
               x > el.x - el.size && 
               x < el.x + el.size) {
             isHotspot = true;
-            updateHotspot(el.x, el.y, 'red');
-            renderHotspots('black');
-            displayDialogue(el);
+            updateHotspot(canvasId, el.x, el.y, 'red', padding);
+            renderHotspots(canvasId, 'black', elements);
+            displayDialogue(canvasId, el);
           }
           else{
             isHotspot = false;
@@ -66,22 +131,22 @@ function checkIfHotspot(e) {
     return isHotspot;
 }
 
-function displayDialogue(el){
-    displayDialogueBox();
-    displayText(el.text, 0, context.canvas.height - 200);
+function displayDialogue(canvasId, el){
+    var originalCanvasHeight = contexts[canvasId].canvas.height;
+    displayText(canvasId, el.text, 0, originalCanvasHeight - 100);
 }
 
-function displayText(text, x, y){
+function displayText(canvasId, text, x, y){
     var words = text.split(' ');
     var line = '';
-    var maxWidth = context.canvas.width;
+    var maxWidth = contexts[canvasId].canvas.width;
 
     for(var n = 0; n < words.length; n++) {
         var testLine = line + words[n] + ' ';
-        var metrics = context.measureText(testLine);
+        var metrics = contexts[canvasId].measureText(testLine);
         var testWidth = metrics.width;
         if (testWidth > maxWidth && n > 0) {
-            context.fillText(line, x, y);
+            contexts[canvasId].fillText(line, x, y);
             line = words[n] + ' ';
             y += 25;
         }
@@ -90,87 +155,48 @@ function displayText(text, x, y){
         }
     }
 
-    context.font = "14pt Georgia";
-    context.fillText(line, x, y);
-    context.fillStyle = "black";
+    contexts[canvasId].font = "14pt Georgia";
+    contexts[canvasId].fillText(line, x, y);
+    contexts[canvasId].fillStyle = "black";
 }
 
-function displayDialogueBox(){
-    context.beginPath();
-    context.strokeStyle = "gray";
-    context.fillStyle = "black";
-    context.fill();
-    context.stroke();
+function refreshCanvas(canvasId, padding) {
+  contexts[canvasId].clearRect(0, 0, contexts[canvasId].canvas.width, contexts[canvasId].canvas.height);
+  contexts[canvasId].drawImage(
+    imageObjs[canvasId], 
+    padding, 
+    padding, 
+    contexts[canvasId].canvas.width - padding*2, 
+    contexts[canvasId].canvas.height - padding*2
+    );
 }
 
-
-function refreshCanvas() {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  context.drawImage(imageObj, 50, 50, context.canvas.width - 100, context.canvas.height - 200 - 100);
-}
-
-function renderHotspots(color) {
+function renderHotspots(canvasId, color, elements) {
     // Render elements.
     elements.forEach(function(element) {
-        drawHotspot(element.x, element.y, element.size, color);
-        displayText(element.name, element.x - 30, element.y)
+        drawHotspot(canvasId, element.x, element.y, element.size, color);
+        displayText(canvasId, element.name, element.x - 30, element.y)
     });
 }
 
-function drawHotspot(x, y, color) {
-  context.beginPath();
-  context.arc(x, y, 100, 0, 2*Math.PI);
-  context.strokeStyle = color;
-  context.stroke();
+function drawHotspot(canvasId, x, y, size, color) {
+  contexts[canvasId].beginPath();
+  contexts[canvasId].arc(x, y , size, 0, 2*Math.PI);
+  contexts[canvasId].strokeStyle = color;
+  contexts[canvasId].lineWidth = 3;
+  contexts[canvasId].stroke();
 }
 
-function drawHotspot(x, y, size, color) {
-  context.beginPath();
-  context.arc(x, y , size, 0, 2*Math.PI);
-  context.strokeStyle = color;
-  context.lineWidth = 3;
-  context.stroke();
+function updateHotspot(canvasId, x, y, color, padding) {
+  refreshCanvas(canvasId, padding);
+  drawHotspot(canvasId, x, y, 50, color);
 }
 
-function updateHotspot(x, y, color) {
-  refreshCanvas();
-  drawHotspot(x, y, 75, color);
-}
-
-function initEvents(events, elements) {
-
-  canvas.addEventListener('mouseover', function(event) {
-    renderHotspots('#0096de');
-  }, false);
-
-  canvas.addEventListener('mouseout', function(event) {
-    refreshCanvas();
-  }, false);
-   
-  // Render events.
-    events.forEach(function(e) {
-      canvas.addEventListener(e, function(event) {
-        var x = event.pageX - canvas.offsetLeft;
-        var y = event.pageY - canvas.offsetTop;
-         console.log(
-              e + 
-              ' @ (' + x + ',' + y + ')'
-            );   
-
-        elements.forEach(function(el) {
-          if (y > el.y - el.size && 
-              y < el.y + el.size && 
-              x > el.x - el.size && 
-              x < el.x + el.size) {
-            console.log(
-                e +
-              ' element:' + 
-              el.name + 
-              ' @ (' + x + ',' + y + ')'
-            );       
-          }
-        });
-      }, false);
-      
-    });
+function initCanvasEvents(canvasId, events) {
+  events.forEach(function(e) {
+    canvases[canvasId].addEventListener(e.name, function(event) {
+      e.result;
+      renderHotspots(canvasId, '#0096de');
+    }, false);
+  });
 }
